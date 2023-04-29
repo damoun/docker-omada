a57d889652f62f689d8b8e78efe86a722d8ba253
Damien Plénard <damien@plenard.me>
2023-04-29 21:05:45 +0200

    add docker image for Omada Controller without mongodb server

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM openjdk:21-jdk-slim-bullseye
+
+RUN mkdir -p /opt/tplink/EAPController/logs
+
+COPY entrypoint.sh /opt/tplink/EAPController/
+COPY properties /opt/tplink/EAPController/properties
+COPY lib /opt/tplink/EAPController/lib
+COPY data /opt/tplink/EAPController/data
+
+RUN ln -s /dev/stdout /opt/tplink/EAPController/logs/server.log
+
+WORKDIR /opt/tplink/EAPController/data
+
+EXPOSE 29811/tcp 29812/tcp 29813/tcp 29814/tcp 8088/tcp 8043/tcp 8843/tcp 29810/udp 27001/udp
+
+ENV OMADA_MANAGE_HTTP_PORT 8088
+ENV OMADA_MANAGE_HTTPS_PORT 8043
+ENV OMADA_PORTAL_HTTP_PORT 8088
+ENV OMADA_PORTAL_HTTPS_PORT 8843
+
+CMD [ "/opt/tplink/EAPController/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+OMADA_HOME=/opt/tplink/EAPController/
+
+echo "eap.mongod.uri=${MONGO_URL}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "manage.http.port=${OMADA_MANAGE_HTTP_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "manage.https.port=${OMADA_MANAGE_HTTPS_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "portal.http.port=${OMADA_PORTAL_HTTP_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "portal.https.port=${OMADA_PORTAL_HTTPS_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
+
+java \
+    -server \
+    -Xms128m -Xmx1024m \
+    -XX:MaxHeapFreeRatio=60 \
+    -XX:MinHeapFreeRatio=30 \
+    -XX:+HeapDumpOnOutOfMemoryError \
+    -Djava.awt.headless=true \
+    -XX:HeapDumpPath=/opt/tplink/EAPController/logs/java_heapdump.hprof \
+    -cp "/usr/share/java/commons-daemon.jar:${OMADA_HOME}/lib/*:${OMADA_HOME}/properties" \
+    com.tplink.smb.omada.starter.OmadaLinuxMain start
