cddbe5388f8ed2e26b6357427898ec5bc9c04cf8
Damien Plénard <damien@plenard.me>
2023-07-02 13:46:26 +0200

    add more env variable to define ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ echo "manage.http.port=${OMADA_MANAGE_HTTP_PORT}" >> "${OMADA_HOME}/properties/o
 echo "manage.https.port=${OMADA_MANAGE_HTTPS_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
 echo "portal.http.port=${OMADA_PORTAL_HTTP_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
 echo "portal.https.port=${OMADA_PORTAL_HTTPS_PORT}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "port.discovery=${OMADA_PORT_DISCOVERY}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "port.adopt.v1=${OMADA_PORT_ADOPT_V1}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "port.upgrade.v1=${OMADA_PORT_UPGRADE_V1}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "port.manager.v1=${OMADA_PORT_MANAGER_V1}" >> "${OMADA_HOME}/properties/omada.properties"
+echo "port.manager.v2=${OMADA_PORT_MANAGER_V2}" >> "${OMADA_HOME}/properties/omada.properties"
 
 java \
     -server \
@@ -17,4 +22,4 @@ java \
     -Djava.awt.headless=true \
     -XX:HeapDumpPath=/opt/tplink/EAPController/logs/java_heapdump.hprof \
     -cp "/usr/share/java/commons-daemon.jar:${OMADA_HOME}/dependency/*:${OMADA_HOME}/lib/*:${OMADA_HOME}/properties" \
-    com.tplink.smb.omada.starter.OmadaLinuxMain start
+    com.tplink.smb.omada.starter.OmadaLinuxMain
